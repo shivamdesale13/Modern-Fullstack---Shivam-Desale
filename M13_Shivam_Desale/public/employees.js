256a77412d125dbeb6f313e2c400c9c41329a80d
Shivam Desale <shivamdesale@Shivams-MacBook-Air.local>
2025-05-04 19:40:37 -0700

    m13

---
+++ M13_Shivam_Desale/public/employees.js
@@ -0,0 +1,13 @@
+let claus = 'Claus Smith';
+let Bobb = 'Bobb Martin';
+let popo = 'popo doss';
+let kaka = 'kaka Shookup';
+let vvss = 'vvss Banks';
+let shivam = 'Shivam Desale';
+const element = /*#__PURE__*/React.createElement("ul", {
+  style: {
+    'color': 'blue',
+    'fontSize': '24px'
+  }
+}, /*#__PURE__*/React.createElement("li", null, sally), /*#__PURE__*/React.createElement("li", null, mark), /*#__PURE__*/React.createElement("li", null, holly), /*#__PURE__*/React.createElement("li", null, amol), /*#__PURE__*/React.createElement("li", null, robin), /*#__PURE__*/React.createElement("li", null, bijal));
+ReactDOM.render(element, document.getElementById('root'));
